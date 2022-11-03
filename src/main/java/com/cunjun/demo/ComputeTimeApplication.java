5d8961727143c0c43cfece2e2e07c82f55747511
zhixin <wangcunjun.wcj@alibaba-inc.com>
2022-11-03 08:53:18 +0800

    fix swagger

--- src/main/java/com/cunjun/demo/ComputeTimeApplication.java
+++ src/main/java/com/cunjun/demo/ComputeTimeApplication.java
@@ -2,10 +2,12 @@ package com.cunjun.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author Cunjun Wang (zhixin) on 2022/11/2
  */
+@EnableSwagger2
 @SpringBootApplication(scanBasePackages = {"com.cunjun.demo"})
 public class ComputeTimeApplication {
 
